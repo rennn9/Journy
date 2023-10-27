bc337458298079e684d33ea33881071b822166cc
Rendi Wijaya <wijayarendi9@gmail.com>
2023-10-27 14:35:01 +0800

    new entry pop-up
    create a pop-up display when you want to create a new entry

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const entryTextarea = document.getElementById('entry');
     const saveButton = document.getElementById('save-entry');
     const entriesDiv = document.querySelector('.entries');
+    const addEntryButton = document.getElementById('add-entry-button');
+    const entryPopup = document.getElementById('entry-popup');
+    const closePopup = document.getElementById('close-popup');
 
     const entries = JSON.parse(localStorage.getItem('diaryEntries')) || [];
 
@@ -10,17 +13,30 @@ document.addEventListener('DOMContentLoaded', function () {
         entries.forEach(entry => {
             const entryDiv = document.createElement('div');
             entryDiv.className = 'entry-box';
-            entryDiv.textContent = entry;
+            const timestampDiv = document.createElement('div');
+            timestampDiv.className = 'timestamp';
+            entryDiv.textContent = entry.text;
+            timestampDiv.textContent = entry.timestamp;
+            entryDiv.appendChild(timestampDiv);
             entriesDiv.appendChild(entryDiv);
         });
     }
+
+    addEntryButton.addEventListener('click', function () {
+        entryPopup.style.display = 'block';
+    });
+
+    closePopup.addEventListener('click', function () {
+        entryPopup.style.display = 'none';
+    });
 
     renderEntries();
 
     saveButton.addEventListener('click', function () {
         const newEntry = entryTextarea.value;
         if (newEntry) {
-            entries.push(newEntry);
+            const timestamp = new Date().toLocaleString();
+            entries.push({ text: newEntry, timestamp: timestamp });
             localStorage.setItem('diaryEntries', JSON.stringify(entries));
             entryTextarea.value = '';
             renderEntries();
